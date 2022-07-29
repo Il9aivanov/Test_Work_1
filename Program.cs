9ea2d4f01c428442c01ada2e7403b75b62acf247
Ilya <il9aivanov@yandex.ru>
2022-07-30 02:09:43 +0300

    add C#_code

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿string[] first_array = { "123", "oil", "24", "joker", "album" };
+int countSTR = 0;
+for (int i = 0; i < first_array.Length; i++)
+{
+    if (first_array[i].Length <= 3)
+    {
+        countSTR++;
+    }
+}
+
+string[] second_array = new string[countSTR];
+string next = String.Empty;
+int second_size = 0;
+
+for (int i = 0; i < first_array.Length; i++)
+{
+    if (first_array[i].Length <= 3)
+    {
+        next = first_array[i];
+        second_array[second_size] = next;
+        second_size++;
+    }
+}
+PrintArray(second_array);
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < second_array.Length; i++)
+    {
+        Console.Write(second_array[i] + " ");
+    }
+}
